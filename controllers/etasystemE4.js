bac5a9e9bb43ffa4aad210126ec5e6831c983cb4
Sam <samueltapping@gmail.com>
2018-04-21 00:26:41 +1000

    more information displayed

--- controllers/etasystemE4.js
+++ controllers/etasystemE4.js
@@ -3,7 +3,13 @@ distance.key(process.env.GOOGLEDISTANCEAPI);
 
 exports.ETAsystem = (req, res) => {
   var origins = ['San Francisco CA'];
-  var destinations = ['New York NY'];
+  var destinations = ['New  York NY'];
+  var modeOfTransport = 'driving';
+  var thingsToAvoid = 'tolls';
+
+  distance.avoid(thingsToAvoid);
+
+  distance.mode(mode);
 
   distance.matrix(origins, destinations, function (err, distances) {
     if (!err) {
